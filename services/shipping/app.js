c0c1652205efd0a47fedf501cfacf0e01319bee2
adlerfaria <adlerfprofissional@gmail.com>
2025-10-29 23:16:51 -0300

    Alterações

--- services/shipping/app.js
+++ services/shipping/app.js
@@ -3,25 +3,38 @@ const path = require('path')
 
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
-async function processMessage(msg) {
-    const deliveryData = JSON.parse(msg.content)
-    try {
-        if(deliveryData.address && deliveryData.address.zipCode) {
-            console.log(`✔ SUCCESS, SHIPPING AUTHORIZED, SEND TO:`)
-            console.log(deliveryData.address)
+var report = {}
+async function updateReport(products) {
+    for (let product of products) {
+        if (!product.name) {
+            continue
+        } else if (!report[product.name]) {
+            report[product.name] = 1;
         } else {
-            console.log(`X ERROR, WE CAN'T SEND WITHOUT ZIPCODE :'(`)
+            report[product.name]++;
         }
-        await (await RabbitMQService.getInstance()).send('report', deliveryData)
+    }
+}
 
-    } catch (error) {
-        console.log(`X ERROR TO PROCESS: ${error.response}`)
+async function printReport() {
+    for (const [key, value] of Object.entries(report)) {
+        console.log(`${key} = ${value} sales`);
     }
 }
 
 async function consume() {
+    async function processMessage(msg) {
+        try {
+            const data = JSON.parse(msg.content)
+            await updateReport(data.products || [])
+            await printReport()
+        } catch (error) {
+            console.log(`X ERROR TO PROCESS: ${error && error.message ? error.message : error}`)
+        }
+    }
+
     console.log(`SUCCESSFULLY SUBSCRIBED TO QUEUE: ${process.env.RABBITMQ_QUEUE_NAME}`)
-    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => {processMessage(msg)})
-} 
+    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => { processMessage(msg) })
+}
 
 consume()
